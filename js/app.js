ec208331059acc2707b4118f785407b80f1998b5
Marta Aparicio <martaaparicio14@gmail.com>
2026-08-04 22:57:50 +0200

    update tempo indicator text

--- js/app.js
+++ js/app.js
@@ -49,7 +49,17 @@ function updateMetronome() {
   tempoDisplay.textContent = bpm;
   tempoSlider.value = bpm;
 
-  // Add conditionals for tempo indicators
+  // Add tempo indicators
+  if (bpm <= 30) { tempoTextString = "Lento" };
+  if (bpm > 30 && bpm < 60) { tempoTextString = "Largo" };
+  if (bpm > 60 && bpm < 66) { tempoTextString = "Larghetto" };
+  if (bpm > 66 && bpm < 80) { tempoTextString = "Adagio" };
+  if (bpm > 80 && bpm < 108) { tempoTextString = "Andante" };
+  if (bpm > 108 && bpm < 120) { tempoTextString = "Moderato" };
+  if (bpm > 120 && bpm < 168) { tempoTextString = "Allegro" };
+  if (bpm > 168 && bpm < 200) { tempoTextString = "Vivace" };
+  if (bpm > 200 && bpm < 252) { tempoTextString = "Presto" };
+  if (bpm >= 252) { tempoTextString = "Prestissimo" };
 
   tempoText.textContent = tempoTextString;
 }
